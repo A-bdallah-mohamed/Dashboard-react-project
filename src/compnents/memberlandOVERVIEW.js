6ef9c4fa5b097499547980c3772b820b0aa75200
git config --global user.name <abddallahmohammed21@gmail.com>
2024-03-16 00:57:36 +0200

    sidebar progress

--- src/compnents/memberlandOVERVIEW.js
+++ src/compnents/memberlandOVERVIEW.js
@@ -5,8 +5,8 @@ export default function MemberlandOVERVIEW() {
     <div className='h-[100vh] flex items-center w-full'>
     <div className='h-[89vh] w-full ml-[45px] mr-[45px]'>
 <div className='font-roboto-condensed font-bold  text-[23px] justify-between flex'>
-    <div>THIS IS MEMBERLAND 👋</div>
-    <div>T3ALA </div>
+    <div className='w-[200px] bg-white pl-5 rounded border-slate-500 border-2'>Member Admin</div>
+    <div><button className='w-[200px] flex items-center justify-center bg-lime-400 rounded text-white border-black border-2 hover:bg-lime-700 hover:' >Add Member +</button> </div>
 </div>
    <div>
 <div className='font-semibold'>HATGEBAK</div>
